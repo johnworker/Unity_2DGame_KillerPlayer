86f7af2ca512e6da60ef837352cc6260bce9bb49
johnworker <78348941+johnworker@users.noreply.github.com>
2023-05-23 20:13:46 +0800

    開始和離開按鈕設定

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;	// 讀取場景需使用的命名空間
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	/// <summary>
+	/// 設定開始方法
+	/// </summary>
+	public void StartGame() 
+	{
+		// 場景管理(資料).LoadScene 讀取場景API ("場景名稱");
+		SceneManager.LoadScene("遊戲場景");
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	/// <summary>
+	/// 設定結束方法
+	/// </summary>
+	public void QuitGame() 
+	{
+		// 應用(資料).Quit();離開內建API
+		Application.Quit();
+	}
 }
