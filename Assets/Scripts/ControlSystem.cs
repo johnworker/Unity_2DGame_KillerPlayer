3448c61616d2753667b2c9c4cfc169719042ed86
johnworker <78348941+johnworker@users.noreply.github.com>
2023-05-25 21:19:34 +0800

    動畫完成

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -33,11 +33,15 @@ public class ControlSystem : MonoBehaviour
 	private void Move()
 	{
 		// GetAxis 取得軸向、Horizontal 水平軸向
+		// 系統判斷 -1 到 1 之間的變化做移動
 		float h = Input.GetAxis("Horizontal"); 
 		float v = Input.GetAxis("Vertical");
 
 		// 物理的加速度，可修改物件身上 Rigidbody2D 的加速度。
 		// 方法必須放入 Update 執行
 		rig.velocity = new Vector2(h, v) * moveSpeed;
+
+		// 設定 h 不等於 0 要走路
+		ani.SetBool(parRun, h != 0 || v !=0);
 	}
 }
