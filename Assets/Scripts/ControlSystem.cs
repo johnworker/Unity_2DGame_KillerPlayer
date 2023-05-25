3d88f325521249a4483387f16fb304fe3cb4bb93
johnworker <78348941+johnworker@users.noreply.github.com>
2023-05-25 19:33:16 +0800

    腳色移動

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -2,20 +2,33 @@ using UnityEngine;
 
 public class ControlSystem : MonoBehaviour
 {
+	[Header("移動速度"), Range(0, 10)]
+	public float moveSpeed = 3.5f;
+
+	public Rigidbody2D rig;
 
 	private void Awake()
 	{
-		print("1 + 2");
-		print($"{ 1 + 2 }");
+		// print("1 + 2");
+		// print($"{ 1 + 2 }");
 	}
 
 	private void Start()
 	{
-		print("<color=blue>開始</color>");
+		// print("<color=blue>開始</color>");
 	}
 
 	private void Update()
 	{
-		print("<color=#996615>執行</color>");
+		// print("<color=#996615>執行</color>");
+
+		Move();
+	}
+
+	private void Move()
+	{
+		// 物理的加速度，可修改物件身上 Rigidbody2D 的加速度。
+		// 方法必須放入 Update 執行
+		rig.velocity = new Vector2(3, 0);
 	}
 }
