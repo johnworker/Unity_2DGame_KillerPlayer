0244160ea58f9b6a3fcd6df0f01e1773dbdee8d4
johnworker <78348941+johnworker@users.noreply.github.com>
2023-06-08 19:20:43 +0800

    生成控制

--- Assets/Scripts/WeaponSpawn.cs
+++ Assets/Scripts/WeaponSpawn.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class WeaponSpawn : MonoBehaviour
 {
+	[Header("生成間隔"), Range(0, 10)]
 	public float interval = 3f;
-
+	[Header("生成武器預置物")]
 	public GameObject prefabWeapon;
+	[Header("武器推力")]
+	public Vector2 power;
 
 	private void SpawnWeapon()
 	{
-		Instantiate(prefabWeapon, transform.position, transform.rotation);
+		GameObject tempWeapon = Instantiate(prefabWeapon, transform.position, transform.rotation);
+
+		Rigidbody2D rigWeapon = tempWeapon.GetComponent<Rigidbody2D>();
 	}
 
 	private void Awake()
