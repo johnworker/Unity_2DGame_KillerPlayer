a716686b3fd3ef7a753edc7242ad37c22b13a8f5
johnworker <78348941+johnworker@users.noreply.github.com>
2023-06-08 20:51:55 +0800

    敵人設定

---
+++ Assets/Scripts/EnemySystem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySystem : MonoBehaviour
+{
+	private Transform player;
+
+	private void Awake()
+	{
+		player = GameObject.Find("衝鋒企鵝").transform;
+	}
+}
