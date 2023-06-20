bff00a316376ca53069b11ba4fc5f75ad3f279fd
johnworker <78348941+johnworker@users.noreply.github.com>
2023-06-20 20:04:05 +0800

    傷害腳本

---
+++ Assets/Scripts/DamageBasic.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class DamageBasic : MonoBehaviour
+{
+	[Header("¸ê®Æ")]
+	public DataBasic data;
+
+	public void Damage()
+	{
+
+	}
+
+	private void Dead()
+	{
+
+	}
+}
