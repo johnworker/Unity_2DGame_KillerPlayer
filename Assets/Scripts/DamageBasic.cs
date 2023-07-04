731fdf077cc180f8b2b90f779df98b605cd8ad63
johnworker <78348941+johnworker@users.noreply.github.com>
2023-07-04 20:10:02 +0800

    掉落經驗值

--- Assets/Scripts/DamageBasic.cs
+++ Assets/Scripts/DamageBasic.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using TMPro;
 
 public class DamageBasic : MonoBehaviour
 {
 	[Header("資料")]
 	public DataBasic data;
+	[Header("傷害值預置物")]
+	public GameObject prefabDamage;
+
 
 	private float hp;
 
@@ -16,13 +20,23 @@ public class DamageBasic : MonoBehaviour
 	{
 		hp -= damage;
 
+		GameObject tempDamage = Instantiate(prefabDamage, transform.position, transform.rotation);
+		// 生成的傷害值物件會儲存在名為 tempDamage 的資料內方便存取
+
+		tempDamage.transform.Find("傷害值文字").GetComponent<TextMeshProUGUI>().text = damage.ToString();
+		// tempDamage.transform.Find("傷害值文字")
+		// 找到生成的傷害值物件 底下 名為&#xff02;傷害值文字&#xff02;的子物件
+
+		// GetComponent<TextMeshProUGUI>().text = damage.ToString();
+		// 取得子物件身上的<TextMeshProUGUI> 文字元件 並更新文字為 傷害值
+
 		// 輸出誰受傷，剩多少血量。
 		print($"<color=#ffee64>{gameObject.name} 血量剩下：{hp}</color>");
 
 		if (hp <= 0) Dead();
 	}
 
-	private void Dead()
+	protected virtual void Dead()
 	{
 		// 輸出誰死亡
 		print($"<color=#ffee64>{gameObject.name} 死亡</color>");
--- Assets/Scripts/DamageEnemy.cs
+++ Assets/Scripts/DamageEnemy.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class DamageEnemy : DamageBasic
 {
-	[Header("傷害值預置物")]
-	public GameObject prefabDamage;
+	private DataEnemy dataEnemy;
+
+	private void Start()
+	{
+		// 將資料轉為敵人資料
+		dataEnemy = (DataEnemy)data;
+		//print(dataEnemy.expProbability);
+	}
 
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
@@ -11,7 +17,17 @@ public class DamageEnemy : DamageBasic
 		{ 
 			Damage(50);
 
-			Instantiate(prefabDamage, transform.position, transform.rotation);
+		}
+	}
+
+	protected override void Dead()
+	{
+		base.Dead();        // 父類別原有的內容
+		Destroy(gameObject);
+
+		if(Random.value < dataEnemy.expProbability)
+		{
+			Instantiate(dataEnemy.prefabExp, transform.position, transform.rotation);
 		}
 	}
 }
