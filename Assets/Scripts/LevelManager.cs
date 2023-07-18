226891bb80958fe6499dd4d7dcddf4f6b6b4fa5d
johnworker <78348941+johnworker@users.noreply.github.com>
2023-07-18 20:16:13 +0800

    升級製作

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -10,6 +10,8 @@ public class LevelManager : MonoBehaviour
 	public TextMeshProUGUI textLv;
 	[Header("文字經驗值")]
 	public TextMeshProUGUI textExp;
+	[Header("升級面板")]
+	public GameObject goLvUp;
 
 	private int lv = 1;
 	private float exp = 0;
@@ -29,9 +31,46 @@ public class LevelManager : MonoBehaviour
 
 	public void AddExp(float exp)
 	{
-		this.exp = exp;
+		this.exp += exp;
 
-		textExp.text = this.exp + " / 100";
-		imgExp.fillAmount = this.exp / 100;
+		if (this.exp > expNeeds[lv -1])
+		{
+			this.exp -= expNeeds[lv - 1];
+			lv++;
+			textLv.text = lv.ToString();
+			LevelUp();
+		}
+
+		textExp.text = this.exp + "/" + expNeeds[lv - 1];
+		imgExp.fillAmount = this.exp / expNeeds[lv - 1];
 	}
+
+	private void LevelUp()
+	{
+		goLvUp.SetActive(true);
+	}
+
+	[ContextMenu("產生經驗值需求資料")]
+	private void ExpNeeds()
+	{
+		expNeeds = new float[100];
+
+		for (int i = 0; i < 100; i++)
+		{
+			expNeeds[i] = (i + 1) * 100 ;
+		}
+	}
+
+	// 執行 10 次並取得每次迴圈的 i 值&#xff1a;從 0 ~ 9
+
+	/*
+	 for (int i = 0; i < 10; i++)
+	{
+		// 迴圈要執行的內容
+	}
+
+	// 小括號內三個區域的意義
+	// int i = 0; 初始值 i 預設為 0
+	// i < 10; 當 i 小於 10 會執行迴圈&#xff0c;否則停止迴圈
+	// i++ 每次迴圈執行完內容後會對 i 加 1*/
 }
