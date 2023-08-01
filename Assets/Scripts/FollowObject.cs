192e2cbcc827b793fb57567577aa0d24648ced32
johnworker <78348941+johnworker@users.noreply.github.com>
2023-08-01 20:05:31 +0800

    玩家受傷

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    [Header("要跟隨的物件")]
+    public Transform target;
+
+    [Header("位移")]
+    public float y = -2.2f;
+
+    private void Update()
+    {
+        Vector3 targetPos = target.position;
+        targetPos.y += y;
+
+        transform.position = targetPos;
+    }
+
+}
